6cbaeaeeb7c60eb832514433289060d9371175fb
Ning Yap <ning.yap@gmail.com>
2013-07-08 19:43:13 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitibikeTripper::Application.config.secret_token = '3ab36f85b644dfabe7fc0e87e6a1bceb4fe7ac089bd88535b2d111dd913bf01b8ed927160c1a595df8725b3f11c3ebd8d39d5a075743c9a5192e01874f6e29de'
