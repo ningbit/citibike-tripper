14152d741b0169de29b2d91fb8e07af193b0e21a
Ning Yap <ning.yap@gmail.com>
2013-07-09 10:54:15 -0400

    devise migration, devise routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :cell_number, :email, :first_name, :home_station, :last_name, :twitter, :work_station
 end
--- db/migrate/20130708234636_create_users.rb
+++ db/migrate/20130708234636_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
+      t.string :email, :null => false, :default => ""
       t.string :twitter
       t.string :cell_number
       t.string :home_station
